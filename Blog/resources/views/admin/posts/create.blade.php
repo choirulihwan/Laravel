93cb6120047c791867e4c3c077fe53fd29cc6937
choirulihwan <aristhu_oracle@yahoo.com>
2019-12-13 20:33:00 +0700

    middleware and group in route done

--- Blog/resources/views/admin/posts/create.blade.php
+++ Blog/resources/views/admin/posts/create.blade.php
@@ -1,5 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-	<h2 class="text-center">Create new post</h2>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h2 class="text-center">Create new post</h2>	
+		</div>
+
+		<div class="panel-body">
+			<form action="{{ route('post.store') }}" method="post">
+
+				{{ csrf_field() }}
+
+				<div class="form-group">					
+					<label for="title">Title</label>
+					<input type="text" class="form-control" name="title">
+				</div>
+
+				<div class="form-group">					
+					<label for="featured">Featured Image</label>
+					<input type="file" class="form-control" name="featured">
+				</div>	
+
+				<div class="form-group">					
+					<label for="content">Content</label>
+					<textarea class="form-control" name="content" id="content" cols="5" rows="5"></textarea>
+				</div>				
+
+				<div class="form-group">					
+					<div class="text-center">
+						<button class="btn btn-success" type="submit">Store post</button>
+					</div>
+				</div>				
+				
+			</form>
+		</div>
+	</div>
+	
 @stop
--- Blog/resources/views/layouts/app.blade.php
+++ Blog/resources/views/layouts/app.blade.php
@@ -75,17 +75,14 @@
         <main class="py-4">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-4">
-                        <ul class="list-group">
-                            <li class="list-group-item">
-                                <a href="/home">Home</a>
-                            </li>
-                            <li class="list-group-item">
-                                <a href="/post/create">Create new post</a>
-                            </li>
-                            
-                        </ul>
-                    </div>
+                    @if(Auth::check())
+                        <div class="col-lg-4">
+                            <ul class="list-group">
+                                <li class="list-group-item"><a href="/home">Home</a></li>
+                                <li class="list-group-item"><a href="{{ route('post.create') }}">Create new post</a></li>
+                            </ul>
+                        </div>
+                    @endif
                     
                     <div class="col-lg-8">
                         @yield('content')
