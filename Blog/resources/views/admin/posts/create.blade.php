5578ff59c0881dba311ee31f93abeb43b7b0ecc9
choirul84 <aristhu_oracle@yahoo.com>
2019-12-14 09:27:58 +0700

    input validation => done

--- Blog/resources/views/admin/posts/create.blade.php
+++ Blog/resources/views/admin/posts/create.blade.php
@@ -1,13 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+
+	@if(count($errors) > 0)
+		<ul class="list-group">
+			@foreach($errors->all() as $error)
+				<li class="list-group-item text-danger">
+					{{ $error }}
+				</li>
+			@endforeach
+		</ul>
+	@endif
+
 	<div class="panel panel-default">
 		<div class="panel-heading">
 			<h2 class="text-center">Create new post</h2>	
 		</div>
 
 		<div class="panel-body">
-			<form action="{{ route('post.store') }}" method="post">
+			<form action="{{ route('post.store') }}" method="post" enctype="multipart/form-data">
 
 				{{ csrf_field() }}
 
