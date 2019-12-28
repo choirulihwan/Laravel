c3f3b6b09ba103b7bec3452af35f12447f4b1189
choirulihwan <aristhu_oracle@yahoo.com>
2019-12-29 06:11:51 +0700

    middleware to check user is admin or not

--- Blog/resources/views/layouts/app.blade.php
+++ Blog/resources/views/layouts/app.blade.php
@@ -87,8 +87,12 @@
                         <div class="col-lg-4">
                             <ul class="list-group">
                                 <li class="list-group-item"><a href="{{ route('home') }}">Home</a></li>
-                                <li class="list-group-item"><a href="{{ route('users') }}">Users</a></li>
+                                
+                                @if(Auth::user()->admin)
+                                <li class="list-group-item"><a href="{{ route('users') }}">List Users</a></li>
                                 <li class="list-group-item"><a href="{{ route('user.create') }}">Create new User</a></li>
+                                @endif
+
                                 <li class="list-group-item"><a href="{{ route('categories') }}">List categories</a></li>
                                 <li class="list-group-item"><a href="{{ route('category.create') }}">Create new category</a></li>
                                 <li class="list-group-item"><a href="{{ route('tags') }}">List tags</a></li>
