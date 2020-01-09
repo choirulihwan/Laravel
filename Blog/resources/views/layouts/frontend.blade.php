d0704d35e9c03e1974e987d058390a0636f9de7c
choirul84 <aristhu_oracle@yahoo.com>
2020-01-09 14:31:53 +0700

    add this social sharing done.

--- Blog/resources/views/layouts/frontend.blade.php
+++ Blog/resources/views/layouts/frontend.blade.php
@@ -159,6 +159,8 @@
 
 
 <!-- ...end JS Script -->
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5e16d5084f164136"></script>
 
 </body>
 </html>
